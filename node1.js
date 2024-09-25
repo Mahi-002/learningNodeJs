197c3c97b726b7ab252c834d6e0da0ceaf2953d2
Mahi <128048403+Mahi-002@users.noreply.github.com>
2024-09-25 21:01:49 +0530

    Add files via upload

--- node1.js
+++ node1.js
@@ -1,5 +1,15 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log('Hello, Mahi here')
+    res.setHeader('Content-Type', 'text/plain');
+    if (req.url === '/home') {
+        res.write('Welcome home');
+      } else if (req.url === '/about') {
+        res.write('Welcome to About Us page');
+      } else if (req.url === '/node') {
+        res.write('Welcome to my Node Js project');
+      } else {
+        res.write('404 Not Found');  
+      }
+      res.end();
 });
 server.listen(4000);
