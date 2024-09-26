bfb0190e0a3fdd4110cc59ead5c7a738fb12f654
Mahi <128048403+Mahi-002@users.noreply.github.com>
2024-09-26 07:08:29 +0530

    Add files via upload

--- node1.js
+++ node1.js
@@ -1,50 +1,6 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        let message = '';
-        if (fs.existsSync('message.txt')) {
-            message = fs.readFileSync('message.txt', 'utf-8');
-        }
-
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body>');
-        if (message) {
-            res.write(`<h3>${message}</h3>`);
-        }
-
-        res.write('<form action="/message" method="POST">');
-        res.write('<input type="text" name="message" required>');
-        res.write('<button type="submit">Send</button>');
-        res.write('</form>');
-        res.write('</body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message); 
-            res.statusCode = 302; 
-            res.setHeader('Location', '/'); 
-            return res.end(); 
-        });
-    }
-});
-
+const routes=require('./routes')
+const server = http.createServer(routes);
 server.listen(4000, () => {
     console.log('Server is listening on port 4000');
 });
