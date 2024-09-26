9837d0435dbffef8f7bf710bf8ec23d8c043b206
Mahi <128048403+Mahi-002@users.noreply.github.com>
2024-09-26 20:42:12 +0530

    Add files via upload

--- node1.js
+++ node1.js
@@ -1,14 +1,27 @@
 const http = require('http');
 const express = require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("in the middleware");
+app.use('/',(req,res,next)=>{
+    console.log("this always runs!");
     next();
+})
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("In another middleware");
+    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add</button></form></body></html>');
 });
-app.use((req,res,next)=>{
-    console.log("in another middleware");
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from express!</h1>');
 });
 
